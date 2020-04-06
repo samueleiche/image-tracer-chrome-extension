53ff0d9e3aa12ed08ef3f6a36c038797fb323ead
Samuel Eiche <7samueleiche@gmail.com>
2020-04-06 14:49:16 +0300

    init

---
+++ background.js
@@ -0,0 +1,8 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.browserAction.onClicked.addListener((tab) => {
+        chrome.tabs.insertCSS(tab.id, { file: 'index.css' })
+        chrome.tabs.executeScript(tab.id, { file: 'vue.min.js' })
+        chrome.tabs.executeScript(tab.id, { file: 'index.js' })
+        chrome.browserAction.disable(tab.id)
+    })
+})

153c02fb500271abe57a65d67b2af0665826cf88
Samuel Eiche <7samueleiche@gmail.com>
2020-04-06 18:54:01 +0300

    remove background onInstalled

--- background.js
+++ background.js
@@ -1,8 +1,6 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.browserAction.onClicked.addListener((tab) => {
-        chrome.tabs.insertCSS(tab.id, { file: 'index.css' })
-        chrome.tabs.executeScript(tab.id, { file: 'vue.min.js' })
-        chrome.tabs.executeScript(tab.id, { file: 'index.js' })
-        chrome.browserAction.disable(tab.id)
-    })
+chrome.browserAction.onClicked.addListener((tab) => {
+    chrome.tabs.insertCSS(tab.id, { file: 'index.css' })
+    chrome.tabs.executeScript(tab.id, { file: 'vue.min.js' })
+    chrome.tabs.executeScript(tab.id, { file: 'index.js' })
+    chrome.browserAction.disable(tab.id)
 })
